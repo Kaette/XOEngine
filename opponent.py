f1d3922ce0e88fd46739ec033563ce159a1fdeee
Kaette <39985997+Kaette@users.noreply.github.com>
2022-05-06 09:40:54 +0200

    create main.py, opponent.py

---
+++ opponent.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+phase = 0
+
+def opening():
+
+def game():
+    
+
+def main():
+    while(phase == 0):
+        opening()
+    while(phase == 1):
+        game()
+
+if __name__ == "__main__":
+    main()
